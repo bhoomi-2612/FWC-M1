7e64ac8409a45bace580a7c6a4b8db29a593c8db
Bhoomika V <12042004bv@gmail.com>
2026-02-11 11:09:39 +0000

    Added PlatformIO structure inside FWC-M1

---
+++ platform/7447/src/main.cpp
@@ -0,0 +1,37 @@
+void disp_7447(int D, int C, int B, int A)
+{
+  digitalWrite(2, A); //LSB
+  digitalWrite(3, B); 
+  digitalWrite(4, C); 
+  digitalWrite(5, D); //MSB
+
+}
+void setup() {
+    pinMode(2, OUTPUT);  
+    pinMode(3, OUTPUT);
+    pinMode(4, OUTPUT);
+    pinMode(5, OUTPUT);
+}
+
+void loop() {
+disp_7447(0,0,0,0);
+delay(1000);
+disp_7447(0,0,0,1);
+delay(1000);
+disp_7447(0,0,1,0);
+delay(1000);
+disp_7447(0,0,1,1);
+delay(1000);
+disp_7447(0,1,0,0);
+delay(1000);
+disp_7447(0,1,0,1);
+delay(1000);
+disp_7447(0,1,1,0);
+delay(1000);
+disp_7447(0,1,1,1);
+delay(1000);
+disp_7447(1,0,0,0);
+delay(1000);
+disp_7447(1,0,0,1);
+delay(1000);
+}
